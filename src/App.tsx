d7768f6cc7d9edf2d28049a9275e0dfe3c0e3558
uttam <uttamreddy73@gmail.com>
2023-10-08 11:05:01 +0100

    changes for movie app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+import ListGroup from "./components/ListGroup";
 
+const API_URL = "http://www.omdbapi.com?apikey=9b7b4bce";
+//9b7b4bce
 function App() {
-  const [count, setCount] = useState(0)
+  const searchMovies = async (title: any) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("pushpa");
+  }, []);
+  return <div></div>;
 }
 
-export default App
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "Japan"];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length == 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
